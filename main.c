cbfec9c6e1472c4b332041c2fea2701f956994f8
chillcode04 <nguyenhuy180904@gmail.com>
2026-04-08 22:45:26 +0700

    add func add()

--- main.c
+++ main.c
@@ -3,9 +3,13 @@
 void change_color() {
     printf("Changing color to blue!\n");
 }
+void add(int a, int b) {
+    printf("The sum of %d and %d is %d\n", a, b, a + b);
+}
 
 int main() {
     printf("Hello, World!\n");
     change_color();
+    add(5, 3);
     return 0;
 }
