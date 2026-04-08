5acba6168ceafca1f640997c5173b59b1dc295c5
chillcode04 <nguyenhuy180904@gmail.com>
2026-04-08 22:59:20 +0700

    add by dev2

--- main.c
+++ main.c
@@ -4,8 +4,13 @@ void change_color() {
     printf("Changing color to blue!\n");
 }
 
+void change_by_dev2() {
+    printf("Function added by dev2 to change color to red!\n");
+}
+
 int main() {
     printf("Hello, World!\n");
     change_color();
+    change_by_dev2();
     return 0;
 }
